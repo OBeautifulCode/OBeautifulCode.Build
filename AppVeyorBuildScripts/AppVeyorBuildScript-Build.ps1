72ec6baa50f64e4b1d896c0021f579bc538d9f59
Suraj Gupta <surajg@gmail.com>
2017-09-15 20:56:31 -0400

    seed the build

---
+++ AppVeyorBuildScripts/AppVeyorBuildScript-Build.ps1
@@ -0,0 +1,61 @@
+# PASTE THIS INTO APPVEYOUR - Build
+
+#######################################################################
+###     Setup variables from Appveyor                               ###
+#######################################################################
+$repoPath = Resolve-Path .
+$buildVersion = $env:appveyor_build_version
+$branchName = $env:appveyor_repo_branch
+$repoName = $env:appveyor_repo_name
+$nugetUrl = $env:nuget_gallery_url
+$nugetKey = $env:nuget_api_key
+$mygetUrl = $env:myget_gallery_url
+$mygetKey = $env:myget_api_key
+
+#######################################################################
+###     Download NuGet tools & Setup NuGet/Artifact scriptblocks    ###
+#######################################################################
+NuGet sources add -Name OBeautifulCodeMyGet -Source https://www.myget.org/F/obeautifulcode-nuget/api/v3/index.json
+$TempBuildPackagesDir = "../TempTools/packages"
+if (-not (Test-Path $TempBuildPackagesDir)) { md $TempBuildPackagesDir | Out-Null }
+$TempBuildPackagesDir = Resolve-Path $TempBuildPackagesDir
+NuGet install 'Naos.Powershell.Build' -OutputDirectory $TempBuildPackagesDir
+$nugetFunctionsScriptPath = $(ls $TempBuildPackagesDir -Recurse | ?{$_.Name -eq 'NuGet-Functions.ps1'}).FullName
+. $nugetFunctionsScriptPath
+
+$nugetScriptblock = { param([string] $fileName) 
+   Write-Host "Pushing $fileName to Build Artifacts"
+   Push-AppveyorArtifact $fileName
+
+   Write-Host "Pushing $fileName to NuGet Gallery"
+   Nuget-PublishPackage -packagePath $fileName -apiUrl $nugetUrl -apiKey $nugetKey
+
+   Write-Host "Pushing $fileName to MyGet Gallery"
+   Nuget-PublishPackage -packagePath $fileName -apiUrl $mygetUrl -apiKey $mygetKey
+}
+
+$artifactScriptBlock = { param([string] $fileName) 
+   Write-Host "Pushing $fileName to Build Artifacts"
+   Push-AppveyorArtifact $fileName
+}
+
+#######################################################################
+###     Pack and push build                                         ###
+#######################################################################
+$createdPackagePaths = New-Object 'System.Collections.Generic.List[String]'
+$informationalVersion = Nuget-CreatePreReleaseSupportedVersion -version $buildVersion -branchName $branchName
+
+$nuSpecFileAnalyzer = Resolve-Path ./Analyzers/OBeautifulCode.Build.Analyzers.nuspec
+$nuSpecFilePackaging = Resolve-Path ./Packaging/OBeautifulCode.Build.Packaging.nuspec
+$nuSpecFileConventionsRepoConfig = Resolve-Path ./Conventions/OBeautifulCode.Build.Conventions.RepoConfig.nuspec
+$nuSpecFileConventionsReSharper = Resolve-Path ./Conventions/OBeautifulCode.Build.Conventions.ReSharper.nuspec
+
+,$nuSpecFileAnalyzer,$nuSpecFilePackaging,$nuSpecFileConventionsRepoConfig,$nuSpecFileConventionsReSharper | %{
+	&$artifactScriptBlock($_)
+	$packageFile = Nuget-CreatePackageFromNuspec -nuspecFilePath $_ -version $informationalVersion -throwOnError $true -outputDirectory $TempBuildPackagesDir
+	$createdPackagePaths.Add($packageFile)
+}
+
+$createdPackagePaths | %{
+	&$nugetScriptblock($_)
+}
---
+++ AppVeyorBuildScripts/AppVeyorBuildScript-Recipes.ps1
@@ -0,0 +1,77 @@
+# PASTE THIS INTO APPVEYOUR - Recipes
+
+###########################################################
+###       Setup variables from Appveyor                 ###
+###########################################################
+$repoPath = Resolve-Path .
+$branchName = $env:appveyor_repo_branch
+$buildVersion = $env:appveyor_build_version
+
+###########################################################
+###    Download and dot source tools to use             ###
+###########################################################
+$TempBuildPackagesDir = "../BuildToolsFromNuGet/packages"
+if (-not (Test-Path $TempBuildPackagesDir)) { md $TempBuildPackagesDir | Out-Null }
+$TempBuildPackagesDir = Resolve-Path $TempBuildPackagesDir
+NuGet install OBeautifulCode.Build -OutputDirectory $TempBuildPackagesDir
+
+$nuSpecTemplateFile = Join-Path (Join-Path (ls $TempBuildPackagesDir/OBeautifulCode.Build.*).FullName 'scripts') 'OBeautifulCodeNuSpecTemplate.template-nuspec'
+
+$nugetFunctionsScriptPath = $(ls $TempBuildPackagesDir -Recurse | ?{$_.Name -eq 'NuGet-Functions.ps1'}).FullName
+
+. $nugetFunctionsScriptPath
+
+#######################################################################
+###     Setup NuGet/Artifact scriptblocks                           ###
+#######################################################################
+$nugetScriptblock = { param([string] $fileName) 
+   Write-Host "Pushing $fileName to Build Artifacts"
+   Push-AppveyorArtifact $fileName
+   Write-Host "Pushing $fileName to NuGet Gallery"
+   Nuget-PublishPackage -packagePath $fileName -apiUrl $env:nuget_gallery_url -apiKey $env:nuget_api_key
+   Write-Host "Pushing $fileName to MyGet Gallery"
+   Nuget-PublishPackage -packagePath $fileName -apiUrl $env:myget_gallery_url -apiKey $env:myget_api_key
+}
+
+$artifactScriptBlock = { param([string] $fileName) 
+   Write-Host "Pushing $fileName to Build Artifacts"
+   Push-AppveyorArtifact $fileName
+}
+
+#######################################################################
+###       Run steps to process recipes repo             ###
+#######################################################################
+
+# if we are in a branch then create a pre-release version for nuget
+$preReleaseSupportVersion = Nuget-CreatePreReleaseSupportedVersion -version $buildVersion -branchName $branchName
+
+# discover the distinct recipes
+$recipes = ls $repoPath | ?{ $_.PSIsContainer } | ?{ -not $_.Name.StartsWith('.') } | %{$_}
+
+# create the nuspec files in place
+$recipes | %{
+	$recipePath = $_.FullName
+	Write-Output ''
+	Write-Output "Creating NuSpec for '$recipePath'"
+	NuGet-CreateRecipeNuSpecInFolder -recipeFolderPath $recipePath -authors 'OBeautifulCode' -nuSpecTemplateFilePath $nuSpecTemplateFile
+}
+
+# push the nuspecs as artifacts
+ls . *.nuspec -Recurse | %{&$artifactScriptBlock($_.FullName)}
+
+# create the nupkgs in place
+$recipes | %{
+	$recipePath = $_.FullName
+	$nuspecFilePath = $(ls $recipePath -Filter "*.$($nuGetConstants.FileExtensionsWithoutDot.Nuspec)").FullName
+	$packageFile = Nuget-CreatePackageFromNuspec -nuspecFilePath $nuspecFilePath -version $buildVersion -throwOnError $true -outputDirectory $recipePath
+}
+
+# push the nupkgs
+$recipes | %{
+	$recipePath = $_.FullName
+	$nuPkgFilePath = $(ls $recipePath -Filter "*.$($nuGetConstants.FileExtensionsWithoutDot.Package)").FullName
+	Write-Output "Pushing package $nuPkgFilePath"
+	&$nugetScriptblock($nuPkgFilePath)
+}
+
+Remove-Item $TempBuildPackagesDir -Recurse -Force
