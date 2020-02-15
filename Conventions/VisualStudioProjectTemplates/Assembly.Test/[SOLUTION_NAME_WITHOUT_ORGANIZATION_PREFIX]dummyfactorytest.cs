c1de53cd0f26757b2ee04603d2f8011479d8e7eb
Suraj Gupta <SurajGupta@users.noreply.github.com>
2020-02-14 21:16:18 -0500

    added dummy factory test class to Assembly.Test vs project template

---
+++ Conventions/VisualStudioProjectTemplates/Assembly.Test/[SOLUTION_NAME_WITHOUT_ORGANIZATION_PREFIX]dummyfactorytest.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="[SOLUTION_NAME_WITHOUT_ORGANIZATION_PREFIX]DummyFactoryTest.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace [PROJECT_NAME]
+{
+    using System;
+
+    using FakeItEasy;
+
+    using OBeautifulCode.AutoFakeItEasy;
+
+    using Xunit;
+
+    public static partial class [SOLUTION_NAME_WITHOUT_ORGANIZATION_PREFIX]DummyFactoryTest
+    {
+    }
+}
