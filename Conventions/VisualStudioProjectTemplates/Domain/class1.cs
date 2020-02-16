73d8b6c80571f24a7f1b99593979533fa2cbecc6
Suraj Gupta <SurajGupta@users.noreply.github.com>
2020-02-16 13:16:53 -0500

    copied Assembly.* into Domain.*

---
+++ Conventions/VisualStudioProjectTemplates/Domain/class1.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Class1.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace [PROJECT_NAME]
+{
+    /// <summary>
+    /// TODO: Starting point for new project.
+    /// </summary>
+    public partial class Class1
+    {
+    }
+}
