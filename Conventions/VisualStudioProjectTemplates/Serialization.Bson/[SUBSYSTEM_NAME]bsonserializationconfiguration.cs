7087a90c8f0f7e2d4dd1c495a1fcd4fe2d2d2dca
Suraj Gupta <SurajGupta@users.noreply.github.com>
2020-02-17 15:33:21 -0500

    added Serialization.Bson project template kind

---
+++ Conventions/VisualStudioProjectTemplates/Serialization.Bson/[SUBSYSTEM_NAME]bsonserializationconfiguration.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="[SUBSYSTEM_NAME]BsonSerializationConfiguration.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace [PROJECT_NAME]
+{
+    using System;
+    using System.Collections.Generic;
+
+    using OBeautifulCode.Serialization.Bson;
+
+    /// <inheritdoc />
+    public class [SUBSYSTEM_NAME]BsonSerializationConfiguration
+    {
+        /// <inheritdoc />
+        protected override IReadOnlyCollection<Type> TypesToAutoRegister => new[]
+        {
+            // ADD TYPES TO REGISTER HERE
+        };
+    }
+}
