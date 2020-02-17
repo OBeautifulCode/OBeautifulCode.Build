e03cbb7d37d92a34d535382befd435435e8d3e08
Suraj Gupta <SurajGupta@users.noreply.github.com>
2020-02-17 15:40:17 -0500

    added Serialization.Json template

---
+++ Conventions/VisualStudioProjectTemplates/Serialization.Json/[SUBSYSTEM_NAME]jsonserializationconfiguration.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="[SUBSYSTEM_NAME]JsonSerializationConfiguration.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace [PROJECT_NAME]
+{
+    using System;
+    using System.Collections.Generic;
+
+    using OBeautifulCode.Serialization.Json;
+
+    /// <inheritdoc />
+    public class [SUBSYSTEM_NAME]JsonSerializationConfiguration : JsonConfigurationBase
+    {
+        /// <inheritdoc />
+        protected override IReadOnlyCollection<Type> TypesToAutoRegister => new[]
+        {
+            // ADD TYPES TO REGISTER HERE
+        };
+    }
+}
