daf4f0e426e9430d2b639a2c2d478db92bdb09d2
Suraj Gupta <SurajGupta@users.noreply.github.com>
2020-02-18 12:31:14 -0500

    capitalization fix

--- Conventions/VisualStudioProjectTemplates/Domain.Test/[SUBSYSTEM_NAME]dummyfactory.cs
+++ Conventions/VisualStudioProjectTemplates/Domain.Test/[SUBSYSTEM_NAME]dummyfactory.cs
@@ -16,7 +16,7 @@ namespace [PROJECT_NAME]
     using OBeautifulCode.AutoFakeItEasy;
 
     /// <summary>
-    /// A dummy factory for types in <see cref="[PROJECT_NAME_WITHOUT_TEST_SUFFIX]"/>.
+    /// A Dummy Factory for types in <see cref="[PROJECT_NAME_WITHOUT_TEST_SUFFIX]"/>.
     /// </summary>
  #if ![RECIPE_CONDITIONAL_COMPILATION_SYMBOL]
     [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
