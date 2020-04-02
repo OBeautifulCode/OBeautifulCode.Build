556a7a1cd17838608d6a40077f8a28b73483db28
Lawson Caudill <lawson@getthinktank.com>
2020-04-02 16:10:00 +0100

    Merged in logic from Naos RepoConfigInstructions that supports preserving localized changed to the solution DotSettings file when applying changes from the templated version during RepoConfig process.

--- Conventions/RepoConfigInstructions.ps1
+++ Conventions/RepoConfigInstructions.ps1
@@ -22,8 +22,58 @@ $slnDotSettingsSourceFilePath = Join-Path $scriptRootPath 'Solution.DotSettings'
 $slnDotSettingsTargetFilePath = $slnFileName + '.DotSettings'
 
 Write-Output "    > Updating Solution DotSettings"
-cp $slnDotSettingsSourceFilePath $slnDotSettingsTargetFilePath -Force
-Write-Output "    - Copied File (cp)"
-Write-Output "    - Source: $slnDotSettingsSourceFilePath"
-Write-Output "    - Target: $slnDotSettingsTargetFilePath"
+$existingFilePresent = Test-Path $slnDotSettingsTargetFilePath
+if ($existingFilePresent)
+{
+    [xml] $oldContentsXml = Get-Content $slnDotSettingsTargetFilePath
+    [xml] $newContentsXml = Get-Content $slnDotSettingsSourceFilePath
+
+    $nodesThatDoNotNeedToBePreserved = $newContentsXml.ChildNodes[1].ChildNodes | ?{($_.Key -ne $null) -and ($_.Key.StartsWith('/Default/UserDictionary/Words/'))}    
+    if ($nodesThatDoNotNeedToBePreserved -eq $null)
+    {
+        $nodesThatDoNotNeedToBePreserved = New-Object 'System.Collections.Generic.List[String]'
+    }
+    
+    $nodesToEvaluateForPreservation = $oldContentsXml.ChildNodes[1].ChildNodes | ?{($_.Key -ne $null) -and ($_.Key.StartsWith('/Default/UserDictionary/Words/'))}
+
+    if ($nodesToEvaluateForPreservation -eq $null)
+    {
+        $nodesForPreservation = New-Object 'System.Collections.Generic.List[String]'
+    }
+    
+    $separatorComment = $newContentsXml.CreateComment(" Preserved customizations are after this comment (do not edit above as it will make diffs more difficult. ")
+    [void]$newContentsXml.DocumentElement.AppendChild($separatorComment)
+    
+    $nodesForPreservation = $nodesToEvaluateForPreservation | ?{($_ -ne $null) -and (-not $nodesThatDoNotNeedToBePreserved.Contains($_))}
+    $nodesForPreservation | %{
+        $newNode = $newContentsXml.ImportNode($_, $true)
+        [void]$newContentsXml.DocumentElement.AppendChild($newNode)
+    }
+    
+    # $settings = New-Object System.Xml.XmlWriterSettings
+    # $settings.Indent = $true
+    # $writer = [System.Xml.XmlWriter]::Create($slnDotSettingsTargetFilePath, $settings);
+    # $newContentsXml.Save($writer);
+    $newContentsXml.Save($slnDotSettingsTargetFilePath)
+    
+    # Cannot figure out how to do this correctly so hacking for newline's and spacing after appending the imported nodes for preservation...
+    $newContentsXmlSavedRaw = Get-Content $slnDotSettingsTargetFilePath
+    $newContentsXmlSavedTreated = $newContentsXmlSavedRaw.Replace('><', ">`n<")
+    $newContentsXmlSavedTreated = $newContentsXmlSavedTreated.Replace("`n<s", "`n	<s")
+    $newContentsXmlSavedTreated = $newContentsXmlSavedTreated.Replace("<!-- Pres", "`n	<!-- Pres")
+    $newContentsXmlSavedTreated | Out-File $slnDotSettingsTargetFilePath -Force
+    
+    Write-Output "    - Found Existing File (merged source into target)"
+    Write-Output "    - Source: $slnDotSettingsSourceFilePath"
+    Write-Output "    - Target: $slnDotSettingsTargetFilePath"
+}
+else
+{
+    cp $slnDotSettingsSourceFilePath $slnDotSettingsTargetFilePath -Force
+
+    Write-Output "    - Copied File (cp)"
+    Write-Output "    - Source: $slnDotSettingsSourceFilePath"
+    Write-Output "    - Target: $slnDotSettingsTargetFilePath"
+}
+
 Write-Output "    < Updating Solution DotSettings"
